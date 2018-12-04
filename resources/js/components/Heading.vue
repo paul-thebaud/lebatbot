4e405e076d909d953d2cd14605c73e105f926b73
Paul Thébaud <paul.thebaud29@gmail.com>
2018-12-04 21:22:37 +0100

    Add link to twitter account

--- resources/js/components/Heading.vue
+++ resources/js/components/Heading.vue
@@ -4,7 +4,8 @@
             <span class="bg-yellow text-black rounded-full px-6 py-2">Le Bat-Bot</span>
         </h1>
         <p>
-            Le bot Twitter qui ajoute <b><i>Bat-</i></b> devant
+            <a href="https://twitter.com/lebatbot/" target="_blank"><b>@LeBatBot</b></a>
+            est un bot Twitter qui ajoute <b><i>Bat-</i></b> devant
             n'importe quel mot du dictionnaire français dans ses Bat-tweets 🦇
         </p>
     </div>
