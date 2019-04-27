ce742e09a88cd3a78abdd5279f18bcd897cda6b8
Paul Thébaud <paul.thebaud29@gmail.com>
2019-04-27 23:04:40 +0200

    Version JS and CSS

--- webpack.mix.js
+++ webpack.mix.js
@@ -18,3 +18,7 @@ mix.js('resources/js/app.js', 'public/js')
         processCssUrls: false,
         postCss: [tailwindcss('./tailwind.js')]
     });
+
+if (mix.inProduction()) {
+    mix.version();
+}
